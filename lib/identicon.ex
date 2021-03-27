9becb9aa3f5ca6520036cad88b455984901de12c
MMcClure11 <mcclme02@luther.edu>
2021-03-27 18:52:38 -0500

    Add draw_image function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -19,6 +19,18 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do 
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) -> 
+      :egd.filledRectangle(image, start, stop, fill) #oddity, updates the the current image, does not create a new one
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do 
