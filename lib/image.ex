4c1aa5bc095ba96aa2f68f2a96e7d68b6778c004
MMcClure11 <mcclme02@luther.edu>
2021-04-05 12:57:53 -0500

    Add documentation and doctest for Identicon.Image

--- lib/image.ex
+++ lib/image.ex
@@ -1,3 +1,16 @@
 defmodule Identicon.Image do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+  Holds the data needed for creating an identicon.
+
+  ## Examples
+
+      iex(1)> %Identicon.Image{}
+      %Identicon.Image{color: nil, grid: nil, hex: nil, pixel_map: nil}
+
+  """
   defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
